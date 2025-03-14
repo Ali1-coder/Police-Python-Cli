b4e4eabfe4af1fe92acf839122327477af0e40ea
Ali1-coder <ablhur3@gmail.com>
2025-03-14 10:23:04 +0300

    1-1 relatioships(Officer-badge,officer-Motorbike)

--- models.py
+++ models.py
@@ -10,17 +10,23 @@ class Officer(Base):
     name = Column(String)
     rank = Column(String)
 
+    badge = relationship("Badge", back_populates="officer", uselist=False)
+    motorbike = relationship("Motorbike", back_populates="assigned_officer", uselist=False)
+
 class Badge(Base):
     __tablename__ = 'badges'
     id = Column(Integer, primary_key=True)
     badge_number = Column(String, unique=True)
     officer_id = Column(Integer, ForeignKey('officers.id'), unique=True) 
 
+    officer = relationship('Officer', back_populates='badge')
+
 class Case(Base):
     __tablename__ = 'cases'
     id = Column(Integer, primary_key=True)
     case_no = Column(String, unique=True)
     details = Column(String)
+    
 
 class Suspect(Base):
     __tablename__ = 'suspects'
@@ -34,3 +40,7 @@ class Motorbike(Base):
     registration_number = Column(String, unique=True)
     model = Column(String)
     status = Column(String, default='Available')
+    officer_id = Column(Integer, ForeignKey('officers.id'))
+
+
+    assigned_officer = relationship("Officer", back_populates="motorbike")
